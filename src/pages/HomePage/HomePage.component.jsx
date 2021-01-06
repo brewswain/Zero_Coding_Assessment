3e9a84157353871b626c06d47ffb06dbe5b3315b
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-06 19:21:03 -0400

    Structured project layout to fit my needs

---
+++ src/pages/HomePage/HomePage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Cart } from "../../components";
+import "./HomePage.style.scss";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
+};
+
+export default HomePage;
