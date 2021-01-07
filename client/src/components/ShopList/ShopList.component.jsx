80b7ed54134781ae21c18c6b620719aa754e16e9
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 13:14:36 -0400

    created test to ensure that frontend can interact with our simple API

--- client/src/components/ShopList/ShopList.component.jsx
+++ client/src/components/ShopList/ShopList.component.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CustomCard } from "..";
 import { shopData } from "../../data/data";
+import shopItemsApi from "../../api/shopItems";
 
 import "./ShopList.style.scss";
 
 const ShopList = () => {
+  //testing data retrieval
+  useEffect(() => {
+    async function getData() {
+      const response = await shopItemsApi.get("/shopItems");
+      console.log(response.data);
+    }
+    getData();
+  }, []);
+
   return (
     <div className="shoplist__container">
       {shopData.map((item) => (
