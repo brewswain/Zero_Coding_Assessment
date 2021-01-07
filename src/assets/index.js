979662e3d16bc360c29ff6faed75105e6a211286
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 10:30:37 -0400

    Edited placeholder images to be the same resolution

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import Cocktail from "./cocktail.png";
+import HeroImage from "./hero_image.png";
+import Roll from "./swiss_roll.png";
+import Salad from "./salad.png";
+import Taco from "./taco.png";
+
+export { Cocktail, HeroImage, Roll, Salad, Taco };
