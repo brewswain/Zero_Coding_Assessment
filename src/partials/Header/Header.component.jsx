a5beae8543b040cc731b8c5b229e1a5b2f7e3267
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 10:31:46 -0400

    Made some basic layout changes

--- src/partials/Header/Header.component.jsx
+++ src/partials/Header/Header.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { selectCart, toggleVisibility } from "../../redux/cart/cartSlice";
+import { useDispatch } from "react-redux";
+import { toggleVisibility } from "../../redux/cart/cartSlice";
 import "./Header.style.scss";
 
 const Header = () => {
-  const cart = useSelector(selectCart);
   const dispatch = useDispatch();
 
   return (
