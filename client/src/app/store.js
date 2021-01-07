ad45def861a46b66159e5c6287572c5e18eee428
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 14:10:19 -0400

    Updated our store to include our shopItemsReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../redux/cart/cartSlice";
+import shopItemsReducer from "../redux/shopItems/shopItemsSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    shopItems: shopItemsReducer,
   },
 });
