afa8b53642f6381b0975c032c63e52a95e3606ba
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-08 18:12:31 -0400

    Becan customizing props available to CustomButton

--- client/src/components/CustomButton/CustomButton.component.jsx
+++ client/src/components/CustomButton/CustomButton.component.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import "./CustomButton.style.scss";
 
-const CustomButton = ({ children, large, ...otherProps }) => (
+const CustomButton = ({ children, large, customClass, ...otherProps }) => (
   <button
-    className={`${large ? "custom__button--large" : ""} custom__button`}
+    className={`${
+      large ? "custom__button--large" : ""
+    } custom__button ${customClass}`}
     // ...otherProps allows us to pass our onClick etc
     {...otherProps}
   >
