d516f9742d8f6174e9b56829ecc802a72cc42653
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 22:48:42 -0400

    Filled out Checkout Page with the layout expected.

--- client/src/pages/CheckoutPage/CheckoutPage.component.jsx
+++ client/src/pages/CheckoutPage/CheckoutPage.component.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CheckoutItem } from "../../components";
+import { selectCartItems } from "../../redux/cart/cartSlice";
 
 import "./CheckoutPage.style.scss";
 
 const CheckoutPage = () => {
-  return <div></div>;
+  const cartItemState = useSelector(selectCartItems);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="checkout__container">
+      <div className="checkout__header">
+        <h1 className="checkout__title">Products</h1>
+        <div className="checkout-options__container">
+          <h1 className="checkout__title">Price</h1>
+          <h1 className="checkout__title">Quantity</h1>
+          <h1 className="checkout__title">Total</h1>
+        </div>
+      </div>
+      {cartItemState.length
+        ? cartItemState.map((cartItem) => (
+            <CheckoutItem key={cartItem._id} item={cartItem} />
+          ))
+        : null}
+    </div>
+  );
 };
 
 export default CheckoutPage;
