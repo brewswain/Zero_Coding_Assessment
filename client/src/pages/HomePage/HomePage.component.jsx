7f001a4554e7e1e4400523f55ee5738c6029688a
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 22:49:27 -0400

    Removed <Header /> from HomePage, and shifted it over to our App.js to reflect routing implentation

--- client/src/pages/HomePage/HomePage.component.jsx
+++ client/src/pages/HomePage/HomePage.component.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { Cart, LandingHero } from "../../components";
-import { Header, ShopList } from "../../partials";
+import { LandingHero } from "../../components";
 import "./HomePage.style.scss";
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
-      <Cart />
       <LandingHero />
-      <ShopList />
     </div>
   );
 };
