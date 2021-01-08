b610179647e7d82b29632a0b0e3443ff21833b47
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-08 18:16:04 -0400

    Update index.js

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,8 +1,21 @@
 import Cocktail from "./IMG/cocktail.png";
 import HeroImage from "./IMG/hero_image.png";
+import Lemons from "./IMG/lemons.jpg";
+import Medley from "./IMG/medley.jpg";
+import Oranges from "./IMG/oranges.jpg";
 import Roll from "./IMG/swiss_roll.png";
 import Salad from "./IMG/salad.png";
 import { ReactComponent as ShoppingCart } from "./SVG/shopping_cart.svg";
 import Taco from "./IMG/taco.png";
 
-export { Cocktail, HeroImage, Roll, Salad, ShoppingCart, Taco };
+export {
+  Cocktail,
+  HeroImage,
+  Lemons,
+  Medley,
+  Oranges,
+  Roll,
+  Salad,
+  ShoppingCart,
+  Taco,
+};
