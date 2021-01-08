edec1e139ebf25f1f37896c4a056691a34de1c93
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-08 00:08:46 -0400

    Edited styling of Landing Hero very slightly

--- client/src/components/LandingHero/LandingHero.component.jsx
+++ client/src/components/LandingHero/LandingHero.component.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import CustomButton from "../CustomButton/CustomButton.component";
-import { HeroImage } from "../../assets";
+// import { HeroImage } from "../../assets";
 
 import "./LandingHero.style.scss";
 
 const LandingHero = () => (
   <div className="landing-hero__container">
     <div className="hero-text__container">
-      <h2 className="landing-hero__title">Hewwo this is a test component</h2>
+      <h2 className="landing-hero__title">Hey this is a test component!</h2>
       <h2 className="landing-hero__title">
-        I might do some simple routing from this hero!
+        I might do some simple routing from this hero
       </h2>
       <Link to="/shop">
-        <CustomButton>Shop Now</CustomButton>
+        <CustomButton large>Shop Now</CustomButton>
       </Link>
     </div>
-    <div className="hero-image__container">
-      <img
-        src={HeroImage}
-        alt="Provided by natasha_t on Unsplash"
-        className="landing-hero__image"
-      />
-    </div>
+    {/* TODO: Implement proper Hero Image On Friday once draft is sent in. */}
+    {/* <div className="hero-image__container"></div> */}
   </div>
 );
 
