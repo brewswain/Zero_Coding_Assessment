396fde554b7fc05f421baee5e99b409c12cdcec1
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 22:48:10 -0400

    To reflect implementation of routing, logic to get shop items from our backend got shifted into our App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import { useDispatch } from "react-redux";
 
-import { HomePage } from "./pages";
+import {
+  getShopItems,
+  // addShopItems,
+} from "./redux/shopItems/shopItemsSlice";
+import { CheckoutPage, HomePage, ShopPage } from "./pages";
+import { Header } from "./partials";
+import { Cart } from "./components";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getShopItems());
+
+    // Left in to show how we added items to our database. Check our shopItemsSlice for more code.
+    // In a more controlled environment, we would probably set up an admin page of some sort to
+    // add the content we need in a less potentially destructive manner, but for now I deemed it
+    // well enough to take our existing data.js file and programmatically add everything to our
+    // database in one fell swoop
+
+    // dispatch(addShopItems());
+  }, [dispatch]);
   return (
     <div className="App">
-      <HomePage />
+      <Header />
+      <Cart />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/checkout" component={CheckoutPage} />
+      </Switch>
     </div>
   );
 }
