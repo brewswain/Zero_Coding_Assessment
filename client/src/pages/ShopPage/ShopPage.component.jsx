8c5bfcbfb9aa7cba476493b4239019a151deee24
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-08 00:07:55 -0400

    Shifted our ShopList component into ShopPage

--- client/src/pages/ShopPage/ShopPage.component.jsx
+++ client/src/pages/ShopPage/ShopPage.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { ShopList } from "../../partials";
 
 import "./ShopPage.style.scss";
 
 const ShopPage = () => {
-  return <div>ShopPage</div>;
+  return (
+    <div>
+      <ShopList />
+    </div>
+  );
 };
 
 export default ShopPage;
