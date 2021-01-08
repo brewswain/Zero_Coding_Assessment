ed818a7155ae26e36e53e07788dfec83b0edaeec
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-07 22:50:31 -0400

    Removed our getShopItems call from ShopList. Moved this logic over to our App.js

--- client/src/partials/ShopList/ShopList.component.jsx
+++ client/src/partials/ShopList/ShopList.component.jsx
@@ -1,29 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  getShopItems,
-  // addShopItems,
-  selectShopItems,
-} from "../../redux/shopItems/shopItemsSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectShopItems } from "../../redux/shopItems/shopItemsSlice";
 import { CustomCard } from "../../components";
 
 import "./ShopList.style.scss";
 
 const ShopList = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getShopItems());
-
-    // Left in to show how we added items to our database. Check our shopItemsSlice for more code.
-    // In a more controlled environment, we would probably set up an admin page of some sort to
-    // add the content we need in a less potentially destructive manner, but for now I deemed it
-    // well enough to take our existing data.js file and programmatically add everything to our
-    // database in one fell swoop
-
-    // dispatch(addShopItems());
-  }, [dispatch]);
-
   const shopItemState = useSelector(selectShopItems);
 
   return (
