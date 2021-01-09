d23a185d86d6e88a8e6339f2f2dd99bddd4bbb2d
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-09 16:49:17 -0400

    Imported my responsive design stylesheets

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./theme/theme.style.scss";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "./theme/theme.style.scss";
+import "./media-queries/1366px.style.scss";
+import "./media-queries/900px.style.scss";
+import "./media-queries/768px.style.scss";
+import "./media-queries/440px.style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
