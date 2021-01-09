9e650dbb20f80bdc003acede4a11e7ae4f91f22d
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-09 12:14:00 -0400

    Updated Landing Page image imports

--- client/src/components/LandingContainer/LandingContainer.component.jsx
+++ client/src/components/LandingContainer/LandingContainer.component.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import CustomButton from "../CustomButton/CustomButton.component";
 import LandingImageBox from "../LandingImageBox/LandingImageBox.component";
-import { Lemons, Medley, Oranges } from "../../assets";
+import { IceCream, Burger, Kebab } from "../../assets";
 
 import "./LandingContainer.style.scss";
 
 const LandingContainer = () => {
   return (
     <div className="landing__container">
-      <LandingImageBox image={Oranges} text="Eden's bounty awaits." />
+      <LandingImageBox image={Kebab} text="Come, we have Tacos!" />
       <LandingImageBox
-        image={Lemons}
-        secondImage={Medley}
-        link="Enter the Garden"
+        image={IceCream}
+        secondImage={Burger}
+        link="Join the Feast"
         linkRoute="/shop"
         left
         text={false}
