59d897a0872141145684ba407900da9528d6fc1e
Brandon Lee <brandonlee2543@hotmail.com>
2021-01-09 16:48:21 -0400

    Cleaned up some unnecessary code

--- server/src/models/shopItem.model.js
+++ server/src/models/shopItem.model.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-// Note, if this was for a production environment I would use the required param,
-// but as I'm just using this Schema to push 4 items that I personally tailored,
-// this is fine.
 const ShopItemSchema = new mongoose.Schema({
   title: {
     type: String,
